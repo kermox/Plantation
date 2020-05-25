d9e0fc0cad1fb5afad00f8b5c3a1da9779b4a92d
Maksym Garus <bboykermo@gmail.com>
2020-05-25 17:10:57 +0200

    test some changes to solve the collectstatic error

--- plantation/settings.py
+++ plantation/settings.py
@@ -114,7 +114,12 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
